0a3180370569a7fb8fff53cfe07bcfcf96c7f5e5
mixaelaavramova <mixaela.avramova@gmail.com>
2020-09-28 08:27:43 +0300

    Tova e upr 2

--- index.php
+++ index.php
@@ -1,33 +1,61 @@
 <?php
-echo "Hello team <br>" ;
+// echo "Hello team <br>" ;
 
-$username = "Mariq";
-echo $username." ";
+// $username = "Mariq";
+// echo $username." ";
 
-$age = 22.38 ;
-echo $age;
+// $age = 22.38 ;
+// echo $age;
 
-$bool = true;
-echo $bool."<br>";
+// $bool = true;
+// echo $bool."<br>";
 
-$array = [1,2,3]; //neasociativen masiv  , moje da slojish masiv v masiv
-echo $array[2]; // 2 indeks  = 3
+// $array = [1,2,3]; //neasociativen masiv  , moje da slojish masiv v masiv
+// echo $array[2]; // 2 indeks  = 3
 
-//asociativen masiv , ima keywrod koito ti stoqn zad imenata 
+// //asociativen masiv , ima keywrod koito ti stoqn zad imenata 
 
-$assocArray = ["username"=>"Mariq <br>","age"=>"22","town"=>"Varna"]; // asociativen masiv pyrviqt string e keywords 
-echo $assocArray["username"];
+// $assocArray = ["username"=>"Mariq <br>","age"=>"22","town"=>"Varna"]; // asociativen masiv pyrviqt string e keywords 
+// echo $assocArray["username"];
 
-echo $assocArray["age"]; 
+// echo $assocArray["age"]; 
 
-echo $assocArray["town"];
+// echo $assocArray["town"];
 
-for($i =0 ; $i<=2 ; $i++)
+// for($i =0 ; $i<=2 ; $i++)
+// {
+//     echo $array[$i]."<br>";
+// }
+
+// foreach($assocArray as $key => $biografiq)
+// {
+//     echo $key." ". $biografiq."<br>";
+// }
+
+$ClassStartSad = 730;
+$ClassStart = 800;
+if ($ClassStartSad > $ClassStart)
 {
-    echo $array[$i]."<br>";
+    echo "Inside";
+}
+elseif($ClassStartSad < $ClassStart)
+{
+    echo "Inside elseif";
+}
+else
+{
+    echo "Inside else";
 }
 
-foreach($assocArray as $key => $biografiq)
+switch ($ClassStart)
 {
-    echo $key." ". $biografiq."<br>";
+case $ClassStart > $ClassStartSad:
+    echo "<br>"."Inside"."<br>";
+    break ;
+case $ClassStart < $ClassStartSad:
+    echo "Inside elseif"."<br>";
+    break;
+default:
+    echo "Match don't find";
+    break;
 }
